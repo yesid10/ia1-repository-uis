a2f2c50c54fa8b42505a284a3e2585b86fa49abc
famarcar <famarcar@saber.uis.edu.co>
2021-11-17 22:52:57 -0500

    tl1

--- talleres20212/convert_ipynb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "uno: $1 dos $2"
-cp $2.ipynb ../$1.ipynb
-if [ ! -f ../$1".ipynb" ]; then
-echo "ERROR!: NO FUE POSIBLE CREAR EL ARCHIVO!."
-else
-echo "¡PERFECTO!, el archivo   '$1.ipynb'   se creó correctamente :D"
-fi
